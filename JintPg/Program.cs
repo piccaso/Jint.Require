2781e48fee7a878acb96fd02f424517ae546595c
Florian Fida <f+git@fida.biz>
2018-02-07 23:41:37 +0100

    Add project files.

---
+++ JintPg/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JintPg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
